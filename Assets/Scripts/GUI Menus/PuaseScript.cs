3d10e3793a0cfca0a1501d098c0d91b568989942
Brady <guyer.brady@gmail.com>
2015-06-10 14:33:26 -0400

    Initial commit

---
+++ Assets/Scripts/GUI Menus/PuaseScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuaseScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
