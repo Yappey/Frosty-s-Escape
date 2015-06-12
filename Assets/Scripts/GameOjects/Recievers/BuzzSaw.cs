76bb6e31b46961d89dc1e2c52f80448e3e076478
Maikikia <mclee@fullsail.edu>
2015-06-12 19:19:31 -0400

    scr_BuzzSaw now has a timer

--- Assets/Scripts/GameOjects/Recievers/BuzzSaw.cs
+++ Assets/Scripts/GameOjects/Recievers/BuzzSaw.cs
@@ -1,37 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuzzSaw : MonoBehaviour {
+public class BuzzSaw : MonoBehaviour
+{
 
-	int _currWayPoint;
-	float _wayPoints = 5;
-	public float _rotationSpeed = 100;
-	public float _movementSpeed = 3;
-	Vector3[] _nextWayPoint = new Vector3[2]; 
+    int             _iCurrWayPoint;
+    bool            _bCanMove                   = false;
+    float           _fWayPoints                 = 5;
+    float           _fMaxTimer                  = 4.0f;
+    float           _fTimerToMove               = 0.0f;
+    public float    _fRotationSpeed             = 100;
+    public float    _fMovementSpeed             = 3;
+    Vector3[]       _vWayPointArray             = new Vector3[2];
 
-	// Use this for initialization
-	void Start () {
-	 _nextWayPoint [0] = new Vector3 (transform.position.x + _wayPoints, 
-		                           transform.position.y, 
-		                           transform.position.z);
-		_nextWayPoint [1] = new Vector3 (transform.position.x - _wayPoints, 
-		                               transform.position.y,
-		                               transform.position.z);
-		_currWayPoint = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.Rotate (Vector3.forward, _rotationSpeed);
+    // Use this for initialization
+    void Start()
+    {
+        _vWayPointArray[0] = new Vector3(transform.position.x + _fWayPoints,
+                                   transform.position.y,
+                                   transform.position.z);
+        _vWayPointArray[1] = new Vector3(transform.position.x - _fWayPoints,
+                                       transform.position.y,
+                                       transform.position.z);
+        _iCurrWayPoint = 0;
+    }
 
-	if (_nextWayPoint[_currWayPoint] == transform.position) {
-			_currWayPoint++;
-			if (_currWayPoint > 1) {
-				_currWayPoint = 0;
-			}
-		}
-		transform.position = Vector3.MoveTowards(transform.position,
-		                                         _nextWayPoint[_currWayPoint],
-		                                         _movementSpeed * Time.deltaTime);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, _fRotationSpeed);
+
+        _fTimerToMove += Time.deltaTime;
+
+        if (_fTimerToMove >= _fMaxTimer)
+        {
+            _bCanMove = true;
+            _fTimerToMove = 0.0f;
+        }
+
+        if (_bCanMove)
+        {
+            if (_vWayPointArray[_iCurrWayPoint] == transform.position)
+            {
+                _bCanMove = false;
+                _iCurrWayPoint++;
+                if (_iCurrWayPoint > 1)
+                {
+                    _iCurrWayPoint = 0;
+                }
+            } 
+        }
+        transform.position = Vector3.MoveTowards(transform.position,
+                                                 _vWayPointArray[_iCurrWayPoint],
+                                                 _fMovementSpeed * Time.deltaTime);
+    }
 }
