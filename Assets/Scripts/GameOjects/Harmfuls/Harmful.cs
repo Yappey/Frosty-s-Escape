ab26a2e9ade5ac864bd39a8b596430676f63b161
Joshua <Joshuagfetsch@gmail.com>
2015-06-12 18:56:24 -0400

    Started Harmful class.
    Still needs work.  Dependent on Health Bar.
    
    Added "Frosty" tag.

--- Assets/Scripts/GameOjects/Harmfuls/Harmful.cs
+++ Assets/Scripts/GameOjects/Harmfuls/Harmful.cs
@@ -3,6 +3,21 @@ using System.Collections;
 
 public class Harmful : MonoBehaviour {
 
+	public enum lifetimeType {
+		none,
+		decay,
+		timed
+	}
+
+	public bool isActive = true;
+	public bool instaKill = false;
+	public bool destroyOnCollision = true;
+	public float damage = 1.0f;
+	public float lifetime = 2.0f;
+	public lifetimeType lifeType = lifetimeType.none;
+
+	float timer = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +25,38 @@ public class Harmful : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (lifeType != lifetimeType.none)
+			timer += Time.deltaTime;
+
+
+		switch (lifeType) {
+		case lifetimeType.none:
+			break;
+		case lifetimeType.decay:
+			if (timer >= lifetime)
+			{
+				if (Random.value < 0.5)
+					Destroy(gameObject);
+				timer = 0.0f;
+			}
+			break;
+		case lifetimeType.timed:
+			if (timer >= lifetime)
+				Destroy(gameObject);
+
+			break;
+		default:
+			break;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Frosty")
+		{
+			// TODO: Hurt Frosty.  Dependent on Health Bar.
+
+
+		}
 	}
 }
