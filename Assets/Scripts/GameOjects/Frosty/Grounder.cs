e1d719230698dc23b4d6408856068377d1b0796e
Joshua <Joshuagfetsch@gmail.com>
2015-06-12 18:09:53 -0400

    Coded Movement/jumps in Frostyehavior.

--- Assets/Scripts/GameOjects/Frosty/Grounder.cs
+++ Assets/Scripts/GameOjects/Frosty/Grounder.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Grounder : MonoBehaviour {
 
+	Frostyehavior frost;
+
 	// Use this for initialization
 	void Start () {
-	
+		frost = GetComponentInParent<Frostyehavior>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	void OnTriggerStay2D(Collider2D col)
+	{
+		frost.isGrounded = true;
+	}
+	
+	void OnTriggerExit2D(Collider2D col)
+	{
+		frost.isGrounded = false;
 	}
 }
