b03ec3f2892899d704988994222dedc148b12427
Joshua <Joshuagfetsch@gmail.com>
2015-06-12 23:15:28 -0400

    Added BaseTurret.
    Update might work with polymorphism.  If it doesn't, use code as reference if needed.

--- Assets/Scripts/GameOjects/Recievers/Turrets/BaseTurret.cs
+++ Assets/Scripts/GameOjects/Recievers/Turrets/BaseTurret.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BaseTurret : MonoBehaviour {
+public class BaseTurret : BaseReceiver {
+
+	public bool isScannig = false;
+	public float frequency = 1.0f;
+	public float projectileVelocity;
+	public float FOV = 30.0f;
+	public float startAngle = 0;
+	public float endAngle = 0;
+
+	float timer = 0.0f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +20,27 @@ public class BaseTurret : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (state != 0)
+		{
+			if (!isScannig)
+			{
+				timer += Time.deltaTime;
+				if (timer >= 1 / frequency)
+				{
+					ShootProjectile();
+				}
+			}
+			else
+			{
+				// TODO: Scanning Functionality - Sprint two
+			}
+		}
 	}
+
+	public virtual void ShootProjectile()
+	{
+
+	}
+
+
 }
