4e0dc489a27598c37fb4f9e52f5dbd7e7b02373f
Doug Sikes <desikes@fullsail.com>
2015-06-12 21:12:01 -0400

    Piston Press moving up and down on a timer ready to be a receiver

--- Assets/Scripts/GameOjects/Recievers/PistonPress.cs
+++ Assets/Scripts/GameOjects/Recievers/PistonPress.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PistonPress : MonoBehaviour {
+public class PistonPress : BaseReceiver {
+
+	private Vector3 upPosition;
+	private float dropTimer;
+	private float raiseTimer;
+
+	private bool grounded = false;
+
+	public float dropTime = 1;
+	public float waitToRaise = 1;
+	public float raiseSpeed = 2;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		dropTimer = dropTime;
+		upPosition = gameObject.transform.position; 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		//if (state) {
+			if (gameObject.transform.position == upPosition) {
+				grounded = false;
+			}
+			
+			if (!grounded && raiseTimer <= 0) {
+				dropTimer -= Time.deltaTime;
+			}
+			
+			
+			if (dropTimer <= 0) {
+				gameObject.GetComponent<Rigidbody2D> ().gravityScale = 1;
+				dropTimer = dropTime;
+			}
+			
+			if (grounded) {
+				raiseTimer -= Time.deltaTime;
+			
+				if (raiseTimer <= 0) {
+					gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, upPosition, raiseSpeed * Time.deltaTime);
+				}
+			}
+		//}
+
+		//else {
+			gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, upPosition, raiseSpeed * Time.deltaTime);
+			grounded = false;
+			dropTimer = 0;
+			gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0;
+		//}
 	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.transform.tag == "Ground") {
+			grounded = true;
+			gameObject.GetComponent<Rigidbody2D>().gravityScale = 0; 
+			raiseTimer = waitToRaise;
+		}
+	}
+
+	//public override void Process() {
+		//if (state == 0) {
+		//	state = 1
+		//}
+
+		//if (state == 1) {
+		//	state = 0
+		//}
+	//}
 }
---
+++ Assets/Scripts/GameOjects/Recievers/PistonPressArm.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistonPressArm : MonoBehaviour {
+
+	public GameObject slam;
+	public GameObject ceiling;
+	private float y;
+	private float yScale;
+
+	// Use this for initialization
+	void Start () {
+		yScale = gameObject.transform.localScale.y;
+
+		y = slam.transform.position.y - ceiling.transform.position.y;
+		y /= 2;
+		y += ceiling.transform.position.y;
+		
+		gameObject.transform.position = new Vector3 (gameObject.transform.position.x , y, gameObject.transform.position.z);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float oldY = y;
+
+		yScale = Mathf.Abs(ceiling.transform.position.y - slam.transform.position.y);
+		yScale /= 2;
+		y = ceiling.transform.position.y - yScale;
+
+		gameObject.transform.position = new Vector3 (gameObject.transform.position.x , y, gameObject.transform.position.z);
+
+		if (y != oldY) {
+			gameObject.transform.localScale = new Vector3 (gameObject.transform.localScale.x , yScale * 2, gameObject.transform.localScale.z);
+		}
+	}
+}
