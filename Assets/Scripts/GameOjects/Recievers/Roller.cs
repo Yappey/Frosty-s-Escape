b5e63719ee7432a2ebedb8563c54717f61f89e3b
Doug Sikes <desikes@fullsail.com>
2015-06-12 23:12:06 -0400

    Started to build the Roller

--- Assets/Scripts/GameOjects/Recievers/Roller.cs
+++ Assets/Scripts/GameOjects/Recievers/Roller.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Roller : MonoBehaviour {
+public class Roller : BaseReceiver {
+
+	public GameObject[] waypoints;
+	public float[] speeds;
+	public float[] waits;
+	public float timer;
+	public bool isMoving = true;
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +17,22 @@ public class Roller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isMoving) {
+			
+		}
+	 	
+		else {
+			if (gameObject.transform.position != waypoints[state].transform.position) {
+				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, waypoints[state].transform.position, speeds[state] * Time.deltaTime);
+			}
+
+			if (gameObject.transform.position == waypoints[state].transform.position) {
+				gameObject.GetComponent<Harmful>().isActive = false;
+			}
+		}
+	}
+
+	public override void Process() {
+		isMoving = !isMoving;
 	}
 }
