3898de2817ca7fa880c321f1b81cd2ef21634107
Joshua <Joshuagfetsch@gmail.com>
2015-06-15 15:53:06 -0400

    Did a little work on the SecurityCamera.cs.  00:30
    Pulling to work on damage stuff.

---
+++ Assets/Scripts/GameOjects/Recievers/SecurityCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecurityCamera : MonoBehaviour {
+
+	public bool isScanning = false;
+	public float FOV = 60.0f;
+	public float startAngle = 0.0f;
+	public float endAngle = 90.0f;
+
+	public Material normalMat;
+	public Material alertedMat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spot()
+	{
+
+	}
+
+	void Lost()
+	{
+
+	}
+}
