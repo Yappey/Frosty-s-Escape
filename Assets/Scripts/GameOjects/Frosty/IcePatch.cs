67cfeedb58f206106193b19f89c51906304db1b9
Brady <guyer.brady@gmail.com>
2015-06-16 15:47:42 -0400

    finished LaserTurret

---
+++ Assets/Scripts/GameOjects/Frosty/IcePatch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IcePatch : MonoBehaviour {
+
+    float time = 0.0f;
+    public float timer;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time >= timer)
+            Destroy(gameObject);
+	}
+}
