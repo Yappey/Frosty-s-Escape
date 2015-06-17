31a5de5550aba6848676f45c354a8cbfecb810ef
Brady <guyer.brady@gmail.com>
2015-06-17 14:33:49 -0400

    Fixed a screwed up Prefabs/GameObjects/Frosty/FrostyFull.prefab

--- Assets/Scripts/GameOjects/Recievers/Turrets/BulletTurret.cs
+++ Assets/Scripts/GameOjects/Recievers/Turrets/BulletTurret.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class BulletTurret : MonoBehaviour {
 
+    public GameObject bullet;
+    public float speed;
+    public float time = 0.0f;
+    public float timer;
+    public float barragetime = 0.0f;
+    public float barragetimer;
+    public int bulletcount = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +18,24 @@ public class BulletTurret : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        time += Time.deltaTime;
+        if(time >= timer)
+        {
+            barragetime += Time.deltaTime;
+            if (barragetime >= barragetimer)
+            {
+                bulletcount++;
+                barragetime = 0.0f;
+                GameObject temp = Instantiate(bullet);
+                temp.transform.position = transform.GetChild(0).position;
+                temp.GetComponent<Bullet>().Velocity = -transform.right * speed; 
+            }
+        }	
+        if(bulletcount == 3)
+        {
+            time = 0;
+            barragetime = 0;
+            bulletcount = 0;
+        }
 	}
 }
