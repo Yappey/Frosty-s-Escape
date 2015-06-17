d9a6a27d44b4a9b766c3466165f7b0573c0252a1
Doug Sikes <desikes@fullsail.com>
2015-06-17 19:21:48 -0400

    Level Select done

---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Linq;
+using System;
+
+public class SoundEffect : MonoBehaviour {
+
+	public AudioSource effect;
+	private float vol;
+	
+	// Use this for initialization
+	void Start () {
+		XElement xRoot = XElement.Load("Volume");
+		XElement xType = xRoot.Element ("Sound");
+		XAttribute xVolume = xType.Attribute("Volume");
+		vol = (float)Convert.ToDouble (xVolume);
+		
+		effect.volume = vol;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		XElement xRoot = XElement.Load("Volume");
+		XElement xType = xRoot.Element ("Sound");
+		XAttribute xVolume = xType.Attribute("Volume");
+		vol = (float)Convert.ToDouble (xVolume);
+		
+		if (effect.volume != vol) {
+			effect.volume = vol;
+		}
+	}
+}
