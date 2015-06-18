a27505f5a60d1ec456e48072a7b5ade5d9831b07
Doug Sikes <desikes@fullsail.com>
2015-06-17 21:57:25 -0400

    vent prefab

--- Assets/Scripts/GameOjects/Activators/Vent.cs
+++ Assets/Scripts/GameOjects/Activators/Vent.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Vent : MonoBehaviour {
+public class Vent : BaseActivator {
+
+	public GameObject exit = null;
+	private GameObject switchmanager;
 
 	// Use this for initialization
 	void Start () {
-	
+		switchmanager = GameObject.FindGameObjectWithTag("SwitchManager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	public override void Activate()
+	{
+		if (exit != null) {
+			GameObject frosty = switchmanager.GetComponent<SwitchManager>().FindActive();
+
+			if (frosty.GetComponent<Frostyehavior>().headAttached &&
+			    !frosty.GetComponent<Frostyehavior>().torsoAttached &&
+			    !frosty.GetComponent<Frostyehavior>().baseAttached &&
+			    frosty.GetComponent<Frostyehavior>().isActive)
+			{
+				frosty.transform.position = exit.transform.position;
+			}
+		}
 	}
 }
