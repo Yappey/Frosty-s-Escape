073337f6a644db3d6123274c997c098a5cd24a80
Maikikia <mclee@fullsail.edu>
2015-06-20 17:14:59 -0400

    Activating the LaserGrid

--- Assets/Scripts/GameOjects/Recievers/BuzzSaw.cs
+++ Assets/Scripts/GameOjects/Recievers/BuzzSaw.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuzzSaw : MonoBehaviour
+public class BuzzSaw : BaseReceiver
 {
 
     int             _iCurrWayPoint;                                     //This is the current index that the vector waypoint is at
+    public bool     _BActive                    = true;
     bool            _bCanMove                   = false;                //A bool for allowing the buzzsaw to move to the new waypoint
     float           _fWayPoints                 = 5;                    //How far the buzzsaws position will increase or decrease 
     float           _fMaxTimer                  = 4.0f;                 //Max amount of time for how long the buzzsaw will wait before moving to new waypoint
@@ -29,47 +30,59 @@ public class BuzzSaw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Rotates the buzzsaw clockwise and counter-clockwise based on the direction it is traveling
-        if (_vWayPointArray[_iCurrWayPoint] == _vWayPointArray[1])
-            transform.Rotate(Vector3.forward, _fRotationSpeed); 
-        else
-            transform.Rotate(Vector3.forward, -_fRotationSpeed); 
-
-        //incrementing the timer
-        _fTimerToMove += Time.deltaTime;    
-
-        //checking to see if the timer is greater than the max time(4 seconds)
-        if (_fTimerToMove >= _fMaxTimer)
+        if (_BActive)
         {
-            //Setting the buzzsaw move to true 
-            _bCanMove = true;
-            //Reseting the timer back to zero to restart the count before buzzsaw can move again
-            _fTimerToMove = 0.0f;
-        }
+            //Rotates the buzzsaw clockwise and counter-clockwise based on the direction it is traveling
+            if (_vWayPointArray[_iCurrWayPoint] == _vWayPointArray[1])
+                transform.Rotate(Vector3.forward, _fRotationSpeed);
+            else
+                transform.Rotate(Vector3.forward, -_fRotationSpeed);
 
-        //check if buzzsaw is allowed to move
-        if (_bCanMove)
-        {
-            //check if buzzsaw position is equal to the waypoint
-            if (_vWayPointArray[_iCurrWayPoint] == transform.position)
+            //incrementing the timer
+            _fTimerToMove += Time.deltaTime;
+
+            //checking to see if the timer is greater than the max time(4 seconds)
+            if (_fTimerToMove >= _fMaxTimer)
             {
-                //setting buzzsaw move back to false
-                _bCanMove = false;
-                //incrementing the index in the waypoint array
-                _iCurrWayPoint++;
+                //Setting the buzzsaw move to true 
+                _bCanMove = true;
+                //Reseting the timer back to zero to restart the count before buzzsaw can move again
+                _fTimerToMove = 0.0f;
+            }
 
-                //check to see if the index is greater than one
-                if (_iCurrWayPoint > 1)
+            //check if buzzsaw is allowed to move
+            if (_bCanMove)
+            {
+                //check if buzzsaw position is equal to the waypoint
+                if (_vWayPointArray[_iCurrWayPoint] == transform.position)
                 {
-                    //reset the index back to 0 to restart the waypoint
-                    _iCurrWayPoint = 0;
+                    //setting buzzsaw move back to false
+                    _bCanMove = false;
+                    //incrementing the index in the waypoint array
+                    _iCurrWayPoint++;
+
+                    //check to see if the index is greater than one
+                    if (_iCurrWayPoint > 1)
+                    {
+                        //reset the index back to 0 to restart the waypoint
+                        _iCurrWayPoint = 0;
+                    }
                 }
-            } 
+            }
+
+            //moves the buzzsaw based on the waypoint
+            transform.position = Vector3.MoveTowards(transform.position,
+                                                     _vWayPointArray[_iCurrWayPoint],
+                                                     _fMovementSpeed * Time.deltaTime); 
         }
-        
-        //moves the buzzsaw based on the waypoint
-        transform.position = Vector3.MoveTowards(transform.position,
-                                                 _vWayPointArray[_iCurrWayPoint],
-                                                 _fMovementSpeed * Time.deltaTime);
+    }
+
+    public override void Process()
+    {
+        _BActive = !_BActive;
+
+        GetComponent<Harmful>().isActive = _BActive;
+
+        base.Process();
     }
 }
--- Assets/Scripts/GameOjects/Recievers/LaserGrid.cs
+++ Assets/Scripts/GameOjects/Recievers/LaserGrid.cs
@@ -3,25 +3,41 @@ using System.Collections;
 
 public class LaserGrid : BaseReceiver
 {
-
-    
+    public bool _BActive = true;
+    bool _instantiate = false;
+    GameObject _LaserGrid;
    
     
     // Use this for initialization
     void Start()
     {
-       
-       
+
+        _LaserGrid = transform.FindChild("Lasers").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+     
     }
 
-    void OnTriggerEnter2D()
+    public override void Process()
     {
+        _BActive = !_BActive;
 
+        GetComponent<Harmful>().isActive = _BActive;
+
+        if (!_BActive)
+        {
+            _LaserGrid.SetActive(false);
+            //DestroyObject(_LaserGrid, 0.1f);
+
+        }
+        else
+        {
+            _LaserGrid.SetActive(true);
+        }
+
+        base.Process();
     }
 }
