4014f7599cb2cd29a786a983a8787dce12197ff3
Joshua <Joshuagfetsch@gmail.com>
2015-07-08 09:48:45 -0400

    Scanning turrets rotate.

---
+++ Assets/Scripts/GameOjects/Recievers/Turrets/SLaserTurret.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SLaserTurret : BaseTurret {
+	
+	// Use this for initialization
+	void Start () {
+		BaseTurretStart();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		BaseTurretUpdate();
+	}
+}
