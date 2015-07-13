b80669bf1cda9117656942c4a3b2eca0c9b4051c
Joshua <Joshuagfetsch@gmail.com>
2015-07-13 10:09:46 -0400

    Reworked Frosty's movement and added ConveyorBelts.

--- Assets/Scripts/GameOjects/Recievers/ConveyorBelt.cs
+++ Assets/Scripts/GameOjects/Recievers/ConveyorBelt.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ConveyorBelt : MonoBehaviour {
+public class ConveyorBelt : BaseReceiver {
+
+	public float[] speeds;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GetComponent<SurfaceEffector2D>().speed = speeds[(state >= 0 && state < speeds.Length) ? state : 0];
+	}
+
+	override public void Process()
+	{
+		state++;
+		if (state >= speeds.Length)
+			state = 0;
 	}
 }
