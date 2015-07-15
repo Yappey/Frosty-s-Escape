b6fb69cac34ec8baa2cb93c99bef45997ff5a63f
Morgan Lee <mclee@fullsail.edu>
2015-07-15 10:15:28 -0400

    Finishing Frosty animations for the head

--- Assets/Scripts/GameOjects/Activators/Button.cs
+++ Assets/Scripts/GameOjects/Activators/Button.cs
@@ -1,27 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Button : BaseActivator {
+public class Button : BaseActivator
+{
 
     public GameObject frosty;
     public GameObject switchmanager;
+   
 
-	// Use this for initialization
-	void Start () {
-		switchmanager = GameObject.FindGameObjectWithTag("SwitchManager");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        switchmanager = GameObject.FindGameObjectWithTag("SwitchManager");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         frosty = switchmanager.GetComponent<SwitchManager>().FindActive();
-	}
+    }
 
     public override void Activate()
     {
         if (frosty.GetComponent<Frostyehavior>().headAttached)
         {
-			GameObject sound = GameObject.FindGameObjectWithTag("SoundEffectManager");
-			sound.GetComponent<SoundEffectManager>().PlayButtonClick();
+            GameObject sound = GameObject.FindGameObjectWithTag("SoundEffectManager");
+            sound.GetComponent<SoundEffectManager>().PlayButtonClick();
 
             foreach (BaseReceiver receiver in receivers)
             {
