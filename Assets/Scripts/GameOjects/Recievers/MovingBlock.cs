e84a5233ddabc9446a05bc76dc19e227dc8d92d2
Joshua <Joshuagfetsch@gmail.com>
2015-07-20 05:44:33 -0400

    Moving Platforms can now be synced with their state.

--- Assets/Scripts/GameOjects/Recievers/MovingBlock.cs
+++ Assets/Scripts/GameOjects/Recievers/MovingBlock.cs
@@ -10,7 +10,10 @@ public class MovingBlock : BaseReceiver {
 	private int currWaypoint = 0;
 	public bool stopImmediately; 
 	public bool cyclical;
+	public bool syncWithState = false;
 	private bool back = false;
+
+	private int dormantState = 0;
 
 
 	// Use this for initialization
@@ -24,11 +27,54 @@ public class MovingBlock : BaseReceiver {
 	
 	// Update is called once per frame
 	void Update () {
-		if (state == 1) {
+		if (state >= 0) {
 			if (gameObject.transform.position == waypoints [currWaypoint].transform.position) {
 				timer += Time.deltaTime;
-				
-				if ((waits.Length == waypoints.Length && timer >= waits [currWaypoint]) || timer >= waits [0]) {
+
+				if (syncWithState)
+				{
+					if (currWaypoint != state)
+					{
+						int dif;
+						if (cyclical)
+						{
+							int posDif = 0, negDif = 0;
+							if (currWaypoint > state)
+							{
+								negDif = currWaypoint - state;
+								posDif = waypoints.Length - currWaypoint + state;
+							}
+							else
+							{
+								negDif = waypoints.Length + currWaypoint - state;
+								posDif = state - currWaypoint;
+							}
+							dif = (posDif <= negDif) ? 1 : -1;
+						}
+						else
+						{
+							dif = (state > currWaypoint) ? 1 : -1;
+						}
+
+						if (dif > 0)
+						{
+							currWaypoint++;
+							if (currWaypoint == waypoints.Length)
+							{
+								currWaypoint = 0;
+							}
+						}
+						else
+						{
+							currWaypoint--;
+							if (currWaypoint == 0)
+							{
+								currWaypoint = waypoints.Length - 1;
+							}
+						}
+					}
+				}
+				else if ((waits.Length == waypoints.Length && timer >= waits [currWaypoint]) || timer >= waits [0]) {
 					timer = 0.0f;
 
 					if (cyclical) {
@@ -66,7 +112,6 @@ public class MovingBlock : BaseReceiver {
 					gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, waypoints [currWaypoint].transform.position, speeds [0] * Time.deltaTime);
 			}
 		}
-		
 		else {
 			if (gameObject.transform.position != waypoints[currWaypoint].transform.position && !stopImmediately)
 				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, waypoints[currWaypoint].transform.position, speeds[currWaypoint] * Time.deltaTime);
@@ -74,10 +119,40 @@ public class MovingBlock : BaseReceiver {
 	}
 
 	public override void Process() {
-		if (state == 1)
-			state = 0;
-
+		if (!syncWithState)
+		{
+			if (state == -1)
+			{
+				state = 0;
+			}
 			else
-				state = 1;
-	}	
+			{
+				state = -1;
+			}	
+		}
+		else
+		{
+			if (state == -1)
+			{
+				state = dormantState;
+			}
+			else
+			{
+				if (cyclical)
+				{
+					state++;
+					if (state == waypoints.Length)
+						state = 0;
+				}
+				else
+				{
+					state += (back) ? -1 : 1;
+					if (state == 0 || state == waypoints.Length - 1)
+					{
+						back = !back;
+					}
+				}
+			}
+		}
+	}
 }
