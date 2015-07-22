fad9a88c929175670008f6e560015fbe59bb1d45
brady <guyer.brady@gmail.com>
2015-07-22 15:16:38 -0400

    asdfasdf

--- Assets/Scripts/GUI Menus/LevelSelectMenuScript.cs
+++ Assets/Scripts/GUI Menus/LevelSelectMenuScript.cs
@@ -11,7 +11,7 @@ public class LevelSelectMenuScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        if(PlayerPrefs.HasKey("ActiveLevel"))
+        if(!PlayerPrefs.HasKey("ActiveLevel"))
         {
             PlayerPrefs.SetInt("ActiveLevvel", 1);
         }
